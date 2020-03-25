e91c0448d244ecee1e2033272f92f5497d033d6d
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-03-25 19:00:44 +0700

    connect api with firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,57 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// const serviceAccount = require("./sociallappp-firebase-adminsdk-ojwss-91c63dcd88.json");
+
+admin.initializeApp({
+  // credential: admin.credential.cert(serviceAccount),
+  // databaseURL: "https://sociallappp.firebaseio.com"
+});
+
+const express = require('express');
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+})
+
+app.post('/scream', (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    })
 });
+
+exports.api = functions.https.onRequest(app);
