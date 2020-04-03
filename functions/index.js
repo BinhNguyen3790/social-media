92863da41dd9f2046eae8d761d63ccdc0df75e3a
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-04-03 22:38:35 +0700

    authentication

--- functions/index.js
+++ functions/index.js
@@ -50,7 +50,40 @@ app.get('/screams', (req, res) => {
     .catch((err) => console.error(err));
 })
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if(req.headers.authorization && req.header.authorization.startsWith('Bearer ')) {
+    idToken = req.headers.authorization.split("Beare ")[1];
+  } else {
+    console.error('No token found');
+    return res.status(403).json({error: "Unauthorized"});
+  }
+  admin.auth().verifyIdToken(idToken) 
+  .then(decodedToken => {
+    req.user = decodedToken;
+    console.log(decodedToken);
+    return db.collection('users')
+    .where("userId", '==', req.user.uid)
+    .limit(1)
+    .get()
+  })
+  .then(data => {
+    req.user.handle = data.docs[0].data().handle;
+    return next();
+  })
+  .catch(err => {
+    console.error('Error while verifying token', err);
+    return res.status(403).json(err);
+  })
+}
+
+app.post('/scream', FBAuth, (req, res) => {
+
+  if(req.body.body.trim() === '') {
+    return res.status(400).json({body: "Body must not be empty"})
+  }
+  
+
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
@@ -69,11 +102,11 @@ app.post('/scream', (req, res) => {
     })
 });
 
-// const isEmail = (email) => {
-//   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   if (email.match(regEx)) return true;
-//   else return false;
-// }
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+}
 
 const isEmpty = (string) => {
   if (string.trim() === '') return true;
@@ -94,7 +127,7 @@ app.post('/signup', (req, res) => {
     errors.email = 'Must not be empty';
   } else if (!isEmail(newUser.email)) {
     errors.email = 'Must be a valid email address';
-  }  
+  }
   if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
   if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
   if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
