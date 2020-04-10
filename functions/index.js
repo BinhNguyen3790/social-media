af09c525051b15a0dc1f1299c68f493d0644ffff
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-04-10 10:20:40 +0700

    getting and commenting on scream

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,17 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const fbAuth = require('./until/fbAuth');
-const { getAllScreams, postOneScream } = require('./handles/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handles/screams');
 const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handles/users');
 
 // Scream routers
 app.get('/screams', getAllScreams);
 app.post('/scream', fbAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+// TODO: delete scream
+// TODO: like scream
+// TODO: unlike scream
+app.post('/scream/:screamId/comment', fbAuth, commentOnScream)
 
 // User route
 app.post('/signup', signUp);
