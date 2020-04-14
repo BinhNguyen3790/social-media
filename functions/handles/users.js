f1f654857420f6d6eeea8b2ff5f9bfdf97750ba7
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-04-14 16:08:09 +0700

    up cloud functions

--- functions/handles/users.js
+++ functions/handles/users.js
@@ -48,22 +48,16 @@ exports.signUp = (req, res) => {
         userId
       };
       db.doc(`/users/${newUser.handle}`).set(userCredentials);
-      // return res.status(201).json({token});
     })
     .then(() => {
       return res.status(201).json({ token });
     })
-
-    // firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-    // .then(data => {
-    //   return res.status(201).json({message: `user ${data.user.uid} signed up successfully`});
-    // }) 
     .catch((err) => {
       console.error(err);
       if (err.code === 'auth/email-already-in-use') {
         return res.status(400).json({ email: 'Email is already is use' });
       } else {
-        return res.status(500).json({ error: err.code });
+        return res.status(500).json({ general: 'Something went wrong, please try again' });
       }
     })
 }
@@ -88,9 +82,9 @@ exports.login = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.code === 'auth/wrong-password') {
+      // if (err.code === 'auth/wrong-password') {
         return res.status(403).json({ general: "Wrong credentials, please try again" })
-      } else return res.status(500).json({ error: err.code });
+      // } else return res.status(500).json({ error: err.code });
     })
 }
 
